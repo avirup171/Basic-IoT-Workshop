30345f647bf1c4ec9e98199dcf6f94ccebe16d22
Avirup Basu <avirup.basu@live.com>
2019-04-05 17:00:45 +0530

    MongoDB updated

--- Lab-1 - Avirup/mongo_DB_test.js
+++ Lab-1 - Avirup/mongo_DB_test.js
@@ -2,7 +2,7 @@
 
 var mqtt    = require('mqtt');
 var MongoClient = require('mongodb').MongoClient;
-
+var url="mongodb://137.117.88.62:27017/";
 var mqttClient= mqtt.connect('mqtt://mqtt.blackspektro.com');
 console.log("MQTT locally connected");
 
@@ -16,14 +16,16 @@ mqttClient.subscribe(topicName);
 
 mqttClient.on('message',function(topic,payload)
 {
-
-	mongoDataPush(payload,collection_name);
+	var obj=JSON.parse(payload);
+	var collection_name=obj.collection_name;
+	mongoDataPush(obj,collection_name);
 });
 
 setInterval(function azcall()
 {
 	var send_message= JSON.stringify({
-		"sent_value":getRandomInt(1,100)
+		"sent_value":getRandomInt(1,100),
+		"collection_name":"nehu_5_4_2019"
 	});
 	mqttClient.publish(topicName, send_message.toString());
 	console.log("Published: " + send_message);
@@ -36,14 +38,16 @@ function getRandomInt(min,max)
 	return Math.floor(Math.random()*(max-min+1))+min;
 }
 
-function mongoDataPush(message)
+function mongoDataPush(message,collection_name)
 {
+	console.log("Entered");
 	MongoClient.connect(url, function(err, db)
 	{
 		if (err) throw err;
 		var dbo = db.db("NEHU");
-		var myobj = message;
-		dbo.collection("test_collection").insertOne(myobj, function(err, res) 
+		var myobj = message
+		console.log(myobj);
+		dbo.collection(collection_name).insertOne(myobj, function(err, res) 
 		{
 			if (err)
 			{
